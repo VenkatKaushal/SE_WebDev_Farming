bc53124b168e1130328578a897f295bfd76fbd4e
VenkatKaushal <cs22b058@iittp.ac.in>
2024-10-22 11:52:02 +0530

    setup for node-backend.

--- web-farm/src/PredictorPage/utils/soilData.js
+++
@@ -1,4 +0,0 @@
-export default [
-    { id: 1, name: 'Loamy Soil', image: 'Loamy.jpeg', url: 'soil.obj', type: 'SOIL'},
-    { id: 2, name: 'Sandy Soil', image: 'Sandy.jpg', url: 'soil.obj' , type: 'SOIL'}
-  ] 
--- web-farm/src/SimulationPage/utils/cropData.js
+++
@@ -1,6 +0,0 @@
-export default [
-    { id: 1, name: 'Wheat', image: 'Wheat.webp', url: 'soil.obj'  },
-    { id: 2, name: 'Rice', image: 'Rice.webp', url: 'soil.obj'  }
-  ];
-  
-  
