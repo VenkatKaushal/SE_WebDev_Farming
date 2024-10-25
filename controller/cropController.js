ace28ca14c99fb4aef5a52e73e70a15d0fe2618e
= <cs22b017@iittp.ac.in>
2024-10-25 19:10:56 +0530

    Minor changes

--- controller/cropController.js
+++ controller/cropController.js
@@ -1,16 +1,21 @@
 // controllers/cropController.js
 
-const Crop = require('../models/crops'); // Adjust the path based on your structure
+// const crops = require('../models/crops');
+const crops = require('../models/crops'); // Adjust the path based on your structure
 
 // @desc Get all crops
 // @route GET /api/crops
 // @access Public
 exports.getMultipleCrops = async (req, res) => {
-        console.log("yes");
+        // console.log("yes");
     try {
         // Fetch all crops from the database
-        const crops = await Crop.find(); // This returns all documents in the crops collection
-        res.status(200).json(crops); // Send the crops data as JSON
+        const crops1 = await crops.find(); 
+     // This returns all documents in the crops collection
+     console.log("Crops Retrieved:", crops1); 
+     
+        res.status(200).json(crops1);
+     // Send the crops data as JSON
     } catch (error) {
         // Handle any errors during the fetching process
         res.status(500).json({ message: 'Error fetching crops', error });
