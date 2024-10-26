d246d27ee6803eaf910c0ea249ae9ff3acb3a690
Hitman-45 <cs22b005@iittp.ac.in>
2024-10-26 17:08:45 +0530

    Translation Feature for multilanguage

--- web-farm/src/HomePage/Footer.jsx
+++ web-farm/src/HomePage/Footer.jsx
@@ -12,6 +12,8 @@ const Footer = () => {
     return (
         <div style={{ backgroundColor: '#e3f2fd'}}>
             <footer className="container py-5">
+                <div id="google_translate_element" style={{ marginBottom: '2rem' }}></div>
+                
                 <div className="row">
                     <div className="col-12 col-md">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="d-block mb-2" role="img" viewBox="0 0 24 24" focusable="false">
@@ -19,6 +21,7 @@ const Footer = () => {
                             <circle cx="12" cy="12" r="10"></circle>
                             <path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"></path>
                         </svg>
+                        <p><a href="mailto:team4temp@gmail.com">team4temp@gmail.com</a></p>
                         <small className="d-block mb-3 text-muted">© 2024-2025</small>
                     </div>
                     <div className="col-6 col-md">
