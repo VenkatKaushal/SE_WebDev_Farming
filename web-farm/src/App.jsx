7764e5b6780953ec92a675a78c848b9373fb4aaa
VenkatKaushal <cs22b058@iittp.ac.in>
2024-11-09 08:39:26 +0530

    Connected to HomePage

--- web-farm/src/App.jsx
+++ web-farm/src/App.jsx
@@ -4,31 +4,17 @@ import SimulationPage from './SimulationPage/SimulationPage'
 import PredictorPage from './Prediction Page/PredictionPage'
 import InfoPage from './Info_Page/Components/info_page'
 import  RegisterPage  from './RegisterPage/Log';
-import CropVisualization from './farm/farm4'
-import CropGrowthSimulator from './farm/farm7'
 
 
 function App(){
-    // return (
-    //     // <App/> Change this name to <App/> and import to see Home page.
-    //     // <HomePage />
-    //     // <CropVisualization/>
-    //     // <PredictorPage/>
-    //     // <InfoPage />
-    //     // <RegisterPage/>
-    //     // <CropVisualization/>
-    //     <CropGrowthSimulator/>
-    // );
     return (
-        <div className="p-4">
-          {/* <CropVisualization /> */}
-          {/* <CropGrowthSimulator/> */}
-          {/* <InteractiveCropSimulator/> */}
-          {/* <PlantVisualization/> */}
-          {/* <CropVisualization/> */}
-          <SimulationPage/>
-        </div>
-      );
+        // <App/> Change this name to <App/> and import to see Home page.
+        <HomePage />
+        // <PredictorPage/>
+        // <InfoPage />
+        // <RegisterPage/>
+        
+    );
 };
 
 export default App;
