15d8d5aa9c8c25f3a604401699705d003b8627ec
VenkatKaushal <cs22b058@iittp.ac.in>
2024-11-10 00:49:36 +0530

    CORS enabling

--- AI Prediction/flask-app.py
+++ AI Prediction/flask-app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 import joblib
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 def predict_crop_yield(input_data):
     try:
