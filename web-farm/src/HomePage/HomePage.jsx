2dac02089e19375681f4580e8e589bb3985d5ed6
VenkatKaushal <cs22b058@iittp.ac.in>
2024-11-16 15:18:08 +0530

    Test1

--- web-farm/src/HomePage/HomePage.jsx
+++ web-farm/src/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./Header";
 import Footer from "./Footer";
-import HomeCarousel from "./HomeCarousel";
+// import HomeCarousel from "./HomeCarousel";
 import LandingPage from "./FirstHomePage";
 
 
