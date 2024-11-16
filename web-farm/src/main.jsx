8d88ab4cd50753c0c1a025883f985a2d7053b660
VenkatKaushal <cs22b058@iittp.ac.in>
2024-11-17 00:35:26 +0530

    Config

--- web-farm/src/main.jsx
+++ web-farm/src/main.jsx
@@ -1,15 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-// import App from './App'
-
 import { RouterProvider } from 'react-router-dom';
-import {router} from './routes/router';
+import { router } from './routes/router'; // Your router setup
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const Main = () => {
+  return (
     <RouterProvider router={router} />
-    {/* <App/> */}
-  </StrictMode>,
-)
+  );
+};
+
+export default Main;
