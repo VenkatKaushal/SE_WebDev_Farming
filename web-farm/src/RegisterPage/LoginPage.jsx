a12bd5634afc2403dd7b03b191ffc1a42f1f798e
VenkatKaushal <cs22b058@iittp.ac.in>
2024-11-17 14:51:09 +0530

    Done

--- web-farm/src/RegisterPage/LoginPage.jsx
+++ web-farm/src/RegisterPage/LoginPage.jsx
@@ -13,7 +13,7 @@ export const LoginPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:3000/api/auth/login', {
+      const res = await axios.post('https://se-webdev-farming.onrender.com/api/auth/login', {
         username,
         email,
         password,
